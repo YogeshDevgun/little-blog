69ac9d4af1ba2e45f62ec52528acd908162ddc7b
Yogesh Devgun <yogeshdevgun@gmail.com>
2017-09-23 20:41:57 +0530

    post display page and fetching data! D structuring variables

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
+import { FETCH_POST } from '../actions';
 
 export default function(state = {}, action){
 
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post
+      // return newState;
+      //make a new key action.payload.data.id and set its value to action.p
+      //payload.data
+      return { ...state, [action.payload.data.id]: action.payload.data};
+
     case FETCH_POSTS:
       //as it will be coming in array we will convert it into object,
       //so while searching through array by for loop we will directly use
@@ -13,6 +23,6 @@ export default function(state = {}, action){
     default:
       return state;
   }
-  
+
 
 }
